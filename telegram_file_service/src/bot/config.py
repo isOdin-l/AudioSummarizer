a9d27ac301401de32385b97c5dc538ed3f616026
Ivan Bildyagin <44323275+isOdin-l@users.noreply.github.com>
2025-12-23 14:11:33 +0300

    Revert "try"

--- telegram_file_service/src/bot/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-import os
-
-class Settings(BaseModel):
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-    API_BASE_URL: str = os.getenv("API_BASE_URL", "http://localhost:8000")
-    SOURCE_TYPE: str = "telegram"
-
-settings = Settings()
